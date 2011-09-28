8e3c8e2133f522c72bf3e7dd60e8a5b6f725bd06
Jim Balhoff <balhoff@nescent.org>
2011-09-28 13:24:58 -0400

    Added data needed for sorting and display to taxon phenotype annotation Solr indexer.

--- src/org/phenoscape/obd/loader/SolrPhenotypeAnnotationLoader.java
+++ src/org/phenoscape/obd/loader/SolrPhenotypeAnnotationLoader.java
@@ -30,15 +30,10 @@ public class SolrPhenotypeAnnotationLoader {
     /** The solr-url system property should contain the url for the Solr web application. */
     public static final String SOLR_URL = "solr-url";
 
-    private static final String ANNOTATIONS_QUERY = "SELECT taxon_annotation.*, phenotype.uid AS phenotype_uid, publication.uid AS publication_uid, EXISTS (SELECT 1 FROM asserted_taxon_annotation WHERE asserted_taxon_annotation.annotation_id = taxon_annotation.annotation_id) AS is_asserted FROM taxon_annotation JOIN phenotype ON (phenotype.node_id = taxon_annotation.phenotype_node_id) LEFT JOIN annotation_source ON (annotation_source.annotation_id = taxon_annotation.annotation_id) LEFT JOIN node publication ON (publication.node_id = annotation_source.publication_node_id)";
+    private static final String ANNOTATIONS_QUERY = "SELECT taxon_annotation.*, phenotype.uid AS phenotype_uid, phenotype.entity_uid, phenotype.entity_label, phenotype.quality_uid, phenotype.quality_label, phenotype.related_entity_uid, phenotype.related_entity_label, taxon.uid AS taxon_uid, taxon.label AS taxon_label, taxon.rank_uid, taxon.is_extinct, EXISTS (SELECT 1 FROM asserted_taxon_annotation WHERE asserted_taxon_annotation.annotation_id = taxon_annotation.annotation_id) AS is_asserted FROM taxon_annotation JOIN phenotype ON (phenotype.node_id = taxon_annotation.phenotype_node_id) JOIN taxon ON (taxon.node_id = taxon_annotation.taxon_node_id)";
 
     private Connection connection;
     private SolrServer solr;
-    private PreparedStatement taxaQuery;
-    private PreparedStatement entitiesQuery;
-    private PreparedStatement qualitiesQuery;
-    private PreparedStatement relatedEntitiesQuery;
-    private PreparedStatement publicationQuery;
 
     public void loadPhenotypeAnnotationsIntoSolr() throws SQLException, ClassNotFoundException, IOException, ParserConfigurationException, SAXException, SolrServerException {
         this.connection = this.getConnection();
@@ -61,9 +56,16 @@ public class SolrPhenotypeAnnotationLoader {
         doc.addField("id", "taxon_annotation_" + annotationsResult.getInt("annotation_id"));
         doc.addField("asserted", annotationsResult.getBoolean("is_asserted"));
         doc.addField("phenotype", annotationsResult.getString("phenotype_uid"));
-        doc.addField("publication", annotationsResult.getString("publication_uid"));
+        doc.addField("direct_taxon", annotationsResult.getString("taxon_uid"));
+        doc.addField("direct_taxon_label", annotationsResult.getString("taxon_label"));
+        doc.addField("is_extinct", annotationsResult.getBoolean("is_extinct"));
+        doc.addField("direct_entity", annotationsResult.getString("entity_uid"));
+        doc.addField("direct_entity_label", annotationsResult.getString("entity_label"));
+        doc.addField("direct_quality", annotationsResult.getString("quality_uid"));
+        doc.addField("direct_quality_label", annotationsResult.getString("quality_label"));
+        doc.addField("direct_related_entity", annotationsResult.getString("related_entity_uid"));
+        doc.addField("direct_related_entity_label", annotationsResult.getString("related_entity_label"));
         return doc;
-
     }
 
     private Connection getConnection() throws SQLException, ClassNotFoundException {
@@ -89,12 +91,12 @@ public class SolrPhenotypeAnnotationLoader {
      * @throws SolrServerException 
      */
     public static void main(String[] args) throws SQLException, IOException, ClassNotFoundException, ParserConfigurationException, SAXException, SolrServerException {
-        //                Logger.getRootLogger().setLevel(Level.ALL);
-        //                final Properties properties = new Properties();
-        //                properties.load(SolrPhenotypeLoader.class.getResourceAsStream("connection.properties"));
-        //                for (Entry<Object, Object> entry : properties.entrySet()) {
-        //                    System.setProperty(entry.getKey().toString(), entry.getValue().toString());
-        //                }
+//        Logger.getRootLogger().setLevel(Level.ALL);
+//        final Properties properties = new Properties();
+//        properties.load(SolrPhenotypeAnnotationLoader.class.getResourceAsStream("connection.properties"));
+//        for (Entry<Object, Object> entry : properties.entrySet()) {
+//            System.setProperty(entry.getKey().toString(), entry.getValue().toString());
+//        }
 
         final SolrPhenotypeAnnotationLoader loader = new SolrPhenotypeAnnotationLoader();
         loader.loadPhenotypeAnnotationsIntoSolr();
