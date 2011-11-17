7fbc7a6d06604a11bd0c5a60552be395de44f64f
Jim Balhoff <balhoff@nescent.org>
2011-11-17 11:30:15 -0500

    Added property loading for connections.

--- test/org/phenoscape/obd/loader/OBDSQLTest.java
+++ test/org/phenoscape/obd/loader/OBDSQLTest.java
@@ -1,15 +1,18 @@
 package org.phenoscape.obd.loader;
 
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Map.Entry;
+import java.util.Properties;
 
 import org.bbop.dataadapter.DataAdapterException;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 
 public class OBDSQLTest {
-    
+
     /** The db-host system property should contain the name of the database server. */
     public static final String DB_HOST = "db-host";
     /** The db-name system property should contain the name of the database. */
@@ -18,19 +21,24 @@ public class OBDSQLTest {
     public static final String DB_USER = "db-user";
     /** The db-password system property should contain the database password. */
     public static final String DB_PASSWORD = "db-password";
-    
+
     private Connection connection;
-    
+
     @BeforeClass
-    public void initialize() throws DataAdapterException, SQLException, ClassNotFoundException {
+    public void initialize() throws DataAdapterException, SQLException, ClassNotFoundException, IOException {
+        final Properties properties = new Properties();
+        properties.load(OBDSQLTest.class.getResourceAsStream("connection.properties"));
+        for (Entry<Object, Object> entry : properties.entrySet()) {
+            System.setProperty(entry.getKey().toString(), entry.getValue().toString());
+        }
         this.connection = this.createConnection();
     }
-    
+
     @AfterClass
     public void destroy() throws SQLException {
         this.connection.close();
     }
-    
+
     private Connection createConnection() throws SQLException, ClassNotFoundException {
         Class.forName("org.postgresql.Driver");
         return DriverManager.getConnection("jdbc:postgresql://" + System.getProperty(DB_HOST) + "/" + System.getProperty(DB_NAME), System.getProperty(DB_USER), System.getProperty(DB_PASSWORD));
@@ -39,5 +47,5 @@ public class OBDSQLTest {
     protected Connection getConnection() {
         return this.connection;
     }
-    
+
 }
